4db8909fb572c4c8174c109cd2070fd9548da915
williamvelichko <williamvelichko2003@gmail.com>
2023-11-10 15:01:21 -0800

    save

--- src/components/smaller-components/PopUp.tsx
+++ src/components/smaller-components/PopUp.tsx
@@ -1,26 +1,26 @@
-// PopUp.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PopUp: React.FC<{ closePopup: () => void }> = ({ closePopup }) => {
   return (
     <div className="md:hidden fixed bg-white border shadow-md w-full">
       <div className="flex flex-col p-4">
-        <a href="/" className="py-2 hover:bg-gray-100" onClick={closePopup}>
+        <Link to="/" className="py-2 hover:bg-gray-100" onClick={closePopup}>
           Home
-        </a>
-        <a href="/schedule" className="py-2 hover:bg-gray-100" onClick={closePopup}>
+        </Link>
+        <Link to="/schedule" className="py-2 hover:bg-gray-100" onClick={closePopup}>
           Schedule
-        </a>
-        <a href="/speakers" className="py-2 hover:bg-gray-100" onClick={closePopup}>
+        </Link>
+        <Link to="/speakers" className="py-2 hover:bg-gray-100" onClick={closePopup}>
           Speakers
-        </a>
-        <a
-          href="/register"
+        </Link>
+        <Link
+          to="/register"
           className="flex justify-center py-2 text-white bg-gray-500 px-4 py-1 rounded-md hover:bg-gray-600 transition duration-300"
           onClick={closePopup}
         >
           REGISTER KNOW
-        </a>
+        </Link>
       </div>
     </div>
   );
