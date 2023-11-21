d6f50e986b94830c6fa0b628caf3da826435c397
williamvelichko <williamvelichko2003@gmail.com>
2023-11-21 12:07:11 -0800

    refactored schedule

--- src/components/pages/SchedulePage.tsx
+++ src/components/pages/SchedulePage.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
 import TitleComponent from '../smaller-components/TitleComponent';
+import scheduleData from '../../data/Schedule.json';
 
 const SchedulePage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-white ">
+    <div className="flex flex-col items-center h-full bg-white">
       <TitleComponent title={'Schedule'} />
-      <div className="w-full h-full flex items-center justify-center m-auto">
-        <div className="flex flex-col justify-evenly w-10/12 h-5/6 p-8 bg-white shadow-lg rounded-lg ">
-          <div className="flex items-center justify-between p-6 border-b border-gray-200">
-            <h2 className="text-xl md:text-2xl lg:text-3xl">10:00 AM</h2>
-            <p className="text-sm md:text-lg">Introduction</p>
-          </div>
-          <div className="flex items-center justify-between p-6 border-b border-gray-200">
-            <h2 className="text-xl md:text-2xl lg:text-3xl">11:00 AM</h2>
-            <p className="text-sm md:text-lg">Session 1</p>
-          </div>
-          <div className="flex items-center justify-between p-6 border-b border-gray-200">
-            <h2 className="text-xl md:text-2xl lg:text-3xl">12:00 PM</h2>
-            <p className="text-sm md:text-lg">Session 2</p>
-          </div>
-          <div className="flex items-center justify-between p-6 border-b border-gray-200">
-            <h2 className="text-xl md:text-2xl lg:text-3xl">1:00 PM</h2>
-            <p className="text-sm md:text-lg">Lunch</p>
-          </div>
-          <div className="flex items-center justify-between p-6 border-b border-gray-200">
-            <h2 className="text-xl md:text-2xl lg:text-3xl">3:00 PM</h2>
-            <p className="text-sm md:text-lg">Session 3</p>
-          </div>
-          <div className="flex items-center justify-between p-6">
-            <h2 className="text-xl md:text-2xl lg:text-3xl">4:00 PM</h2>
-            <p className="text-sm md:text-lg">Session 4</p>
-          </div>
+      <div className="w-full h-full flex items-center justify-center py-8">
+        <div className="flex flex-col justify-evenly sm:w-10/12 w-11/12 h-full p-8  bg-white shadow-lg rounded-lg">
+          {scheduleData.map((item, index) => (
+            <div
+              key={index}
+              className={`flex sm:flex-row flex-col items-center justify-between p-6 ${
+                index !== scheduleData.length - 1 ? 'border-b border-gray-200' : ''
+              }`}
+            >
+              <h2 className="text-xl md:text-2xl lg:text-3xl">{item.time}</h2>
+              <p className="text-sm md:text-lg sm:m-0 mt-2">{item.title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
