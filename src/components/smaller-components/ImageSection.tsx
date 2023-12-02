713466100b40625ebbbd5f63521f26302b4bb2ee
williamvelichko <williamvelichko2003@gmail.com>
2023-12-01 16:35:58 -0800

    save

--- src/components/smaller-components/ImageSection.tsx
+++ src/components/smaller-components/ImageSection.tsx
@@ -1,37 +1,29 @@
 import React from 'react';
+import images from '../../data/Images.json'; // Adjust the path as per your file structure
 
 const ImageSection: React.FC = () => {
   return (
-    <div className="w-full h-full flex justify-center mb-4">
-      <div className="flex flex-col md:flex-row justify-evenly w-full md:space-x-4">
-        <div className="flex flex-col items-center justify-center space-y-4 mb-4 md:mb-0">
-          <div className="w-full h-96 relative">
-            <img src="/bibleStudy.jpg" alt="Placeholder" className="w-full h-full object-cover " />
-            <h3 className="text-white text-4xl md:text-4xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              Equipping
-            </h3>
+    <div className="w-full h-full flex flex-col justify-center items-center mt-8 p-4 bg-gray-100">
+      <div className="flex flex-wrap justify-between md:w-10/12 w-11/12 mt-20">
+        {images.map((image, index) => (
+          <div key={index} className="w-full sm:w-1/2 xl:w-1/2 p-2">
+            <div className="w-full h-96 relative">
+              <img src={image.src} alt={image.alt} className="w-full h-full object-cover" />
+              <div className="flex flex-col justify-end items-start absolute inset-0 bg-black bg-opacity-30 p-4 text-white">
+                <h3 className="text-4xl md:text-4xl font-bold">{image.text}</h3>
+              </div>
+            </div>
           </div>
-          <div className="w-full h-96 relative">
-            <img src="/fellowship.jpg" alt="Placeholder" className="w-full h-full object-cover" />
-            <h3 className="text-white text-4xl md:text-4xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              Fellowship
-            </h3>
-          </div>
-        </div>
-        <div className="flex flex-col items-center justify-center space-y-4">
-          <div className="w-full h-96 relative">
-            <img src="/coffee.jpg" alt="Placeholder" className="w-full h-full object-cover" />
-            <h3 className="text-white text-4xl md:text-4xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              Refreshments
-            </h3>
-          </div>
-          <div className="w-full h-96 relative">
-            <img src="/truth.jpg" alt="Placeholder" className="w-full h-full object-cover" />
-            <h3 className="text-white text-4xl md:text-4xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              Truth
-            </h3>
-          </div>
-        </div>
+        ))}
+      </div>
+
+      {/* Section for Purpose and Mission */}
+      <div className="flex flex-col w-10/12 text-center my-24 p-4">
+        <h2 className="text-xl font-bold mb-4">Purpose & Mission</h2>
+        <p className="text-sm">
+          Welcome to Faithful Steward Conference, where men are nurtured in truth and empowered as stewards of faith
+          through the Scriptures, Discipleship, and Fellowship.
+        </p>
       </div>
     </div>
   );
